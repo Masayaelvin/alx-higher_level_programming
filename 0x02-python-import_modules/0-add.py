6b0389b19f81e542ed5ef546022e3661baeaa7a0
Masayaelvin <masayaelvin@gmail.com>
2023-01-08 01:18:54 -0800

    python modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
 
+"""print the sum of 1 and 2"""
+
 a = 1
 b = 2
 
-c = add(a,b)
-print("{} + {} = {}\n".format(a,b,c))
+c = add(a, b)
+print("{} + {} = {}\n".format(a, b, c))
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def add(a,b):
-        """My addition function
-            Args:
-                   a: first integer
-                   b: second integer
-                   Returns: the return value. a + b
     """
+    this function calculates the sum of two values a,b
+
+    Args:
+        a: first interger
+        b: second interger
+
+    Returns:
+        the return value. a + b
+        """
     return (a + b)
