49f4059dc97352c0a96b817e538076e03ab53afc
Masayaelvin <masayaelvin@gmail.com>
2023-01-08 01:04:53 -0800

    python modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/bin/bash/python3
+from add_0 import add
+
+a = 1
+b = 2
+
+c = add(a,b)
+print("{} + {} = {}\n".format(a,b,c))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,9 @@
+#!/bin/bash/python3
+def add(a,b):
+        """My addition function
+            Args:
+                   a: first integer
+                   b: second integer
+                   Returns: the return value. a + b
+    """
+    return (a + b)
