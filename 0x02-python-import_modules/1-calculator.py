79ecec4a2706ea0572569aebf18551b65c4d5869
Masayaelvin <masayaelvin@gmail.com>
2023-01-08 01:56:34 -0800

    python modules

---
+++ 0x02-python-import_modules/1-calculator.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+
+    from calculator_1.py import add, sub, mul, div
+    """ this code gives the value of the sum , product, quotient'
+    and differece of 10 and 5"""
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b))
+    print("{} + {} = {}".format(a, b, sub(a, b))
+    print("{} + {} = {}".format(a, b, mul(a, b))
+    print("{} + {} = {}".format(a, b, div(a, b))
