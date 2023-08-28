5af0b87882da5260506b9972a9a695ef2b9d2aa2
Masayaelvin <masayaelvin@gmail.com>
2023-08-28 07:12:29 -0700

    python exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,9 +4,8 @@
 def safe_print_division(a, b):
     try:
         quo = a / b
-        return (quo)
     except (ZeroDivisionError, TypeError):
         quo = None
-        return (quo)
     finally:
         print("Inside result:{}".format(quo))
+    return (quo)
