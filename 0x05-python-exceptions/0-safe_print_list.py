2315e8e30e116dd1fa376308a2cf5cb4aa0d3c54
Masayaelvin <masayaelvin@gmail.com>
2023-08-28 05:39:05 -0700

    python exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-#safe priting elvin
+# safe priting elvin
 
 def safe_print_list(my_list=[], x=0):
     n = 0
     try:
         while n < x:
-            print("{}".format(my_list[n]), end ="")
-            n =+ 1
+            print("{}".format(my_list[n]), end="")
+            n += 1
     except IndexError:
         break
