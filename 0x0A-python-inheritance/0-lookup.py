4cc848391fd3f40a5bbd52030634f3fa234f4149
Masayaelvin <masayaelvin@gmail.com>
2023-09-11 11:34:22 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''returns available attributes of the obj'''
 
-def lookup(obj):
 
+def lookup(obj):
     """Return a list of an object's available attributes."""
     return dir(obj)
