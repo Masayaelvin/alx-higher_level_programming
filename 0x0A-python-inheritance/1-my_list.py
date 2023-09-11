b62f77f5176f3759a11fba567b19f3bbb658b9ad
Masayaelvin <masayaelvin@gmail.com>
2023-09-11 11:38:55 -0700

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class MyList(list):
+    "this is a list class that inherits from list"
+
+
+    def print_sorted(self):
+        """sorts an unsorted list"""
+        return sorted(list)
