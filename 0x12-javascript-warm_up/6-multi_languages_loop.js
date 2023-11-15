7f3258ae2c7ee488c9eb314713d71f7f5e91b468
Masayaelvin <masayaelvin@gmail.com>
2023-11-15 07:07:37 -0800

    correction_6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+let arr =["C is fun","Python is cool","JavaScript is amazing"];
+
+let x = 0;
+
+while (x<=arr.length-1){
+  console.log(arr[x]);
+  x++;
 }
