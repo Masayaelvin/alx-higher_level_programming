7b2dd481b9b68de69c449af15c1e58caafd06e79
Masaya Elvin <masayaelvin@gmail.com>
2024-01-04 13:03:31 +0300

    fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,6 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const request = require('request');
-
 const id = process.argv[2];
-
 request.get(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, response, body) => {
   if (error) {
     console.error(error);
